1c2187de19be05697beeb7dd1f3ec71626ee9818
Pokey Rule <755842+pokey@users.noreply.github.com>
2025-10-03 15:06:19 +0100

    feat(langchain/createAgent): Add support for callOptions to middleware

--- libs/langchain/src/agents/middlewareAgent/nodes/AgentNode.ts
+++ libs/langchain/src/agents/middlewareAgent/nodes/AgentNode.ts
@@ -251,7 +251,7 @@ export class AgentNode<
     options: {
       lastMessage?: string;
     } = {}
-  ): Promise<AIMessage | ResponseHandlerResult<StructuredResponseFormat>> {
+  ) {
     const model = await this.#deriveModel();
 
     /**
@@ -290,7 +290,11 @@ export class AgentNode<
     }
 
     const signal = mergeAbortSignals(this.#options.signal, config.signal);
-    const invokeConfig = { ...config, signal };
+    const invokeConfig = {
+      ...config,
+      signal,
+      ...preparedOptions?.callOptions,
+    };
     const response = (await modelWithTools.invoke(
       modelInput,
       invokeConfig
